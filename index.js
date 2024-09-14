ca9d888b3af48f976584641b14813635c9ec278a
shafeeque <shafeeqsha06@gmail.com>
2024-09-14 12:59:24 +0530

    feat(resolvers): explored resolver functions to qury data from QraphQL

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema.js";
 
+import db from "./_db.js";
 //server setup
-const server = ApolloServer({
+
+const resolvers = {
+    Query: {
+        games() {
+            return db.games;
+        },
+        authors() {
+            return db.authors; 
+        },
+        reviews() {
+            return db.reviews;
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
 
 });
 
