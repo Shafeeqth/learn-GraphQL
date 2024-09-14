dc5f09bd89980a7a6e969841ca1d520611db501c
shafeeque <shafeeqsha06@gmail.com>
2024-09-14 13:19:56 +0530

    feat: parametrized query for single doc

--- schema.js
+++ schema.js
@@ -19,8 +19,12 @@ type Author {
 
 type Query {
     reviews: [Review]
+    review(id: ID!): Review
     games: [Game]
+    game(id: ID!): Game
     authors: [Author]
+    author(id: ID!): Author
+
 }
 
 `
