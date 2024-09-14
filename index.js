aecaf08e7d12b186f62bee3d4d43e05343e0d624
shafeeque <shafeeqsha06@gmail.com>
2024-09-14 11:10:23 +0530

    feat(server): setup basic appolo server

---
+++ index.js
@@ -0,0 +1,15 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+//server setup
+const server = ApolloServer({
+
+});
+
+const {url} = await startStandaloneServer(server, {
+    listen: {
+        port: 4000
+    }
+});
+
+console.log('server ready at port', 4000);
